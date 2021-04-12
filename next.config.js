50b3ff0b90958b8dae69aacee191c7a619ad33c0
Ricardo Gonzalez <rickgc@gmail.com>
2021-04-12 11:27:36 +0100

    ⬆️ Update modules

--- next.config.js
+++ next.config.js
@@ -1,34 +1,4 @@
-const { createSecureHeaders } = require('next-secure-headers');
-
 module.exports = {
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: createSecureHeaders({
-          contentSecurityPolicy: {
-            directives: {
-              defaultSrc: ["'self'"],
-              styleSrc: ["'self'", "'unsafe-inline'"],
-              imgSrc: ["'self'"],
-              baseUri: 'self',
-              formAction: 'self',
-              frameAncestors: true
-            }
-          },
-          frameGuard: 'deny',
-          noopen: 'noopen',
-          nosniff: 'nosniff',
-          xssProtection: 'sanitize',
-          forceHTTPSRedirect: [
-            true,
-            { maxAge: 60 * 60 * 24 * 360, includeSubDomains: true }
-          ],
-          referrerPolicy: 'same-origin'
-        })
-      }
-    ];
-  },
   poweredByHeader: false,
   future: {
     webpack5: true,
